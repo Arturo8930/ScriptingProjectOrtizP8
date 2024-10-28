0229aacc9011bcb4609c8c828b0da13a63a910c2
Arturo Ortiz <753788@student.dusd.net>
2024-10-28 15:04:04 -0700

    Created Loop Scripts
    Created 4 Scripts for my Unity Project.

---
+++ ScriptingProjectP8Ortiz/Assets/Scripts/DoWhileLoops.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoops : MonoBehaviour
+{
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello World");
+
+        }while (shouldContinue == true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
