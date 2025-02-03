848bdf5cf5125e22dbd9351e626f44e6325b86ee
Arturo Ortiz <753788@student.dusd.net>
2025-02-03 14:38:01 -0800

    Linear Interpolation (Extra)
    just added something to my script

--- ScriptingProjectP8Ortiz/Assets/Scripts/LinearInterpolation.cs
+++ ScriptingProjectP8Ortiz/Assets/Scripts/LinearInterpolation.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class LinearInterpolation : MonoBehaviour
 {
+    /*
+
+    //In this case, result = 4
+
+    float result = Mathf.Lerp (3f, 5f, 0.5f);
+
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+
+    //Here result = (4, 5, 6)
+    Vector3 result = Vector3.Lerp (from, to, 0.75f);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,8 @@ public class LinearInterpolation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
     }
+*/
 }
